855daee8413ec0eb61e465512dd72d281c9f7b35
BELKADI <khaera/belkadi@gmail.com>
2023-01-20 19:02:27 +0100

    Showing

--- hrNet-frontend/src/components/Table/TableEmployees.jsx
+++ hrNet-frontend/src/components/Table/TableEmployees.jsx
@@ -48,14 +48,14 @@ const TableEmployees = ({ ids }) => {
                         dataSource={filterData == null ? data : filterData} 
                         size='middle' 
                         pagination={{
-                            style:{marginTop:'30px'},
+                            style:{marginTop:'40px'},
                             defaultPageSize:10, 
                             defaultCurrent:1,
                             showSizeChanger:true,
                             size: 'small',
                             position: ['bottomCenter'],
-                            showAllData: (pages, range) => (
-                                `Showing ${range[0]} to ${range[1]} of ${pages} entries`
+                            showTotal: (total, range) => (
+                                `Showing ${range[0]} to ${range[1]} of ${total} entries`
                             )
                         }} 
                         scroll={{y: 500}} 
